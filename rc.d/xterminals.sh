9503cc10d58cb7a7302e1febc6f187bcde068345
Chris Aumann <me@chr4.org>
2014-04-14 20:00:29 +0200

    Enable window title setting for tmux

--- rc.d/xterminals.sh
+++ rc.d/xterminals.sh
@@ -1,20 +1,32 @@
 case ${TERM} in
-  xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix)
+  xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix|screen*)
+
+    # Set tmux pane title, if TERM is screen*
+    # We cannot use $TMUX for the check, as it is not set on remote systems
+    set_tmux_title() {
+      case ${TERM} in screen*)
+        printf "\033k$1\033\\"
+      esac
+    }
+
     if [ -n "$ZSH_VERSION" ]; then
       # there's no HOSTNAME variable on zsh
       HOSTNAME=$(hostname)
 
       function precmd() {
         echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD)\007"
+        set_tmux_title "${HOSTNAME%%.*} | $(basename $PWD)"
       }
 
       function preexec() {
         echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD) | $1\007"
+        set_tmux_title "${HOSTNAME%%.*} | $(basename $PWD) | $1"
       }
     fi
 
     if [ -n "$BASH" ]; then
       PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD)\007"'
+      set_tmux_title "${HOSTNAME%%.*} | $(basename $PWD)"
     fi
   ;;
 esac
