134c4f039b4a2dd167cce8b00ea6a1253834c2a5
Chris Aumann <me@chr4.org>
2014-04-23 14:23:05 +0200

    Revert "Enable window title setting for tmux"
    tmux configuration uses pane-names, which are set to normal xterminal
    titles
    
    This reverts commit 9503cc10d58cb7a7302e1febc6f187bcde068345.
    
    Conflicts:
        rc.d/xterminals.sh

--- rc.d/xterminals.sh
+++ rc.d/xterminals.sh
@@ -1,21 +1,11 @@
 case ${TERM} in
   xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix|screen*)
-
-    # Set tmux pane title, if TERM is screen*
-    # We cannot use $TMUX for the check, as it is not set on remote systems
-    set_tmux_title() {
-      case ${TERM} in screen*)
-        printf "\033k$1\033\\"
-      esac
-    }
-
     if [ -n "$ZSH_VERSION" ]; then
       # There's no HOSTNAME variable on zsh
       HOSTNAME=$(hostname)
 
       function precmd() {
         echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD)\007"
-        set_tmux_title "${HOSTNAME%%.*} | $(basename $PWD)"
       }
 
       function preexec() {
@@ -23,13 +13,11 @@ case ${TERM} in
         PROGRAM=$(echo $1 |cut -f1 -d' ')
 
         echo -ne "\033]0;${HOSTNAME%%.*} | $PROGRAM\007"
-        set_tmux_title "${HOSTNAME%%.*} | $PROGRAM"
       }
     fi
 
     if [ -n "$BASH" ]; then
       PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*}\007"'
-      set_tmux_title "${HOSTNAME%%.*}"
     fi
   ;;
 esac
