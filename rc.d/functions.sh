cd09094043a93f7bc00d1205d322fdefceefeb39
Chris Aumann <me@chr4.org>
2014-07-03 10:49:45 +0200

    Add psgrep function

--- rc.d/functions.sh
+++ rc.d/functions.sh
@@ -4,3 +4,6 @@ chr() { printf $(printf '\\%03o\\n' "$1"); }
 
 # highlight text
 highlight() { e="$1"; shift; grep --color=always -Eih "$e|$" "$@"; }
+
+# grep process table
+psgrep() { psc | grep -v grep | grep "$@" -i --color=auto; }
