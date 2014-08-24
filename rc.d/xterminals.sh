7b0a7601af4db94631023da62caf88e9d84a256e
Chris Aumann <me@chr4.org>
2014-08-24 15:46:29 +0200

    Only prepend hostname on remote machines [zsh]

--- rc.d/xterminals.sh
+++ rc.d/xterminals.sh
@@ -1,15 +1,22 @@
 case ${TERM} in
   xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix|screen*)
+    # If current terminal is an ssh session, prepend hostname to prompt
+    remote_host() {
+      if [ -n "$SSH_CONNECTION" ]; then
+        echo "$(hostname) | "
+      fi
+    }
+
     if [ -n "$ZSH_VERSION" ]; then
       function precmd() {
-        echo -ne "\033]0;$(hostname) | $(basename $PWD)\007"
+        echo -ne "\033]0;$(remote_host)$(basename $PWD)\007"
       }
 
       function preexec() {
         # Strip argument list from command
         PROGRAM=$(echo $1 |cut -f1 -d' ')
 
-        echo -ne "\033]0;$(hostname) | $PROGRAM\007"
+        echo -ne "\033]0;$(remote_host)${PROGRAM}\007"
       }
     fi
 
