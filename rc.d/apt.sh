6ea4efed4b9fcd709ee0243cfc97b186e60508b0
Chris Aumann <me@chr4.org>
2014-08-26 13:58:51 +0200

    Evaluate apt aliases on runtime

--- rc.d/apt.sh
+++ rc.d/apt.sh
@@ -1,8 +1,8 @@
 # Create useful aliases for apt cleanup commands
 if which apt-get &> /dev/null; then
   # Remove old kernels (but the most recent, and the currently running one
-  alias apt-purge-old-kernels="apt-get purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{print $2}' |tr '[:space:]' ' ')"
+  alias apt-purge-old-kernels="apt-get purge \$(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print \$3,\$2}' |sort -nr |tail -n +2 |grep -v \$(uname -r) |awk '{print \$2}' |tr '[:space:]' ' ')"
 
   # Purge removed packages
-  alias apt-purge-removed-packages="apt-get purge -y $(dpkg -l |grep '^rc' |awk '{print $2}' |tr '[:space:]' ' ')"
+  alias apt-purge-removed-packages="apt-get purge -y \$(dpkg -l |grep '^rc' |awk '{print \$2}' |tr '[:space:]' ' ')"
 fi
